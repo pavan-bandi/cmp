f0a4725d9899cfcc3d640642298ccb5025f9b5a9
pavan-bandi <109429441+pavan-bandi@users.noreply.github.com>
2022-10-14 18:42:24 +0530

    Update settings.py

--- cmp/settings.py
+++ cmp/settings.py
@@ -75,12 +75,11 @@ WSGI_APPLICATION = 'cmp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'jntu',
-        'USER':'root',
-        'PASSWORD':'Geetha'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
