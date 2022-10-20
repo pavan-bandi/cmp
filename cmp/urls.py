7e6a5cf92e535eafba99e7e3271d94572403879b
pavan-bandi <109429441+pavan-bandi@users.noreply.github.com>
2022-10-21 00:58:15 +0530

    Update urls.py

--- cmp/urls.py
+++ cmp/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -22,7 +22,7 @@ from django.views.static import serve
 
 urlpatterns = [path('',include('clmt.urls')),
     path('admin/', admin.site.urls),
-               url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-               url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+             
+
 
                ]
