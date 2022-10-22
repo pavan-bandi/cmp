eb6c37ca4166f378d5d0a2d828aeb4da4da698ac
pavan-bandi <109429441+pavan-bandi@users.noreply.github.com>
2022-10-22 11:41:05 +0530

    Update settings.py

--- cmp/settings.py
+++ cmp/settings.py
@@ -71,8 +71,8 @@ TEMPLATES = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-     
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
