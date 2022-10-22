0fd68050ddc8f6aa17b974b4de057805b42d98be
pavan-bandi <109429441+pavan-bandi@users.noreply.github.com>
2022-10-22 11:37:06 +0530

    Update settings.py

--- cmp/settings.py
+++ cmp/settings.py
@@ -89,7 +89,14 @@ WSGI_APPLICATION = 'cmp.wsgi.application'
 # ie if Heroku server
 if 'DATABASE_URL' in os.environ:
     import dj_database_url
-    DATABASES = {'default': dj_database_url.config()}
+   DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jntu',
+        'USER':'root',
+        'PASSWORD':'Geetha'
+    }
+}
 
 
 # Password validation
