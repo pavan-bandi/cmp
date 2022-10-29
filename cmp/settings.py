864a066bd2d813c9cdaa9a91c7d43a588c3910fd
pavan-bandi <109429441+pavan-bandi@users.noreply.github.com>
2022-10-29 22:29:13 +0530

    Update settings.py

--- cmp/settings.py
+++ cmp/settings.py
@@ -81,9 +81,13 @@ WSGI_APPLICATION = 'cmp.wsgi.application'
 
 # ie if Heroku server
 
-if 'DATABASE_URL' in os.environ:
-    import dj_database_url
-    DATABASES = {'default': dj_database_url.config()}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
