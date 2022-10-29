faf364ba733a3ca6a4cfb3d21a814e5f6a1ea4fd
pavan-bandi <109429441+pavan-bandi@users.noreply.github.com>
2022-10-30 02:31:32 +0530

    Update settings.py

--- cmp/settings.py
+++ cmp/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
 }
 import dj_database_url
 
-    DATABASES = {'default': dj_database_url.config()}
+DATABASES = {'default': dj_database_url.config()}
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
