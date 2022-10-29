95936f3389bbaac1048b81821c1454f418074910
pavan-bandi <109429441+pavan-bandi@users.noreply.github.com>
2022-10-29 23:54:25 +0530

    Update settings.py

--- cmp/settings.py
+++ cmp/settings.py
@@ -83,13 +83,17 @@ WSGI_APPLICATION = 'cmp.wsgi.application'
 
 # ie if Heroku server
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dudhsagar',
+       'USER':'root',
+        'PASSWORD':'Geetha',
+         "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
