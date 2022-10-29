66159b4582930f38631a2ea56221d1eaa67a20d4
pavan-bandi <109429441+pavan-bandi@users.noreply.github.com>
2022-10-29 22:31:21 +0530

    Update settings.py

--- cmp/settings.py
+++ cmp/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import dj-database-url 
+import psycopg2
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
