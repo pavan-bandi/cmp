f7aaf943923854cfd27229e4c6028de79d901dde
pavan-bandi <109429441+pavan-bandi@users.noreply.github.com>
2022-10-30 01:30:29 +0530

    Update settings.py

--- cmp/settings.py
+++ cmp/settings.py
@@ -87,11 +87,11 @@ WSGI_APPLICATION = 'cmp.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        'Host':'ec2-18-209-78-11.compute-1.amazonaws.com'
-'NAME':'du73pd7tmoarr'
-'USER':'vdcxymshuhzkrr'
-'PORT':'5432'
-'PASSWORD':'1c2f4caf3bfd048c0eb237464c9112285978747d2fe2894329952af54f35e35f'
+        'Host':'ec2-18-209-78-11.compute-1.amazonaws.com',
+'NAME':'du73pd7tmoarr',
+'USER':'vdcxymshuhzkrr',
+'PORT':'5432',
+'PASSWORD':'1c2f4caf3bfd048c0eb237464c9112285978747d2fe2894329952af54f35e35f',
    
     }
 }
