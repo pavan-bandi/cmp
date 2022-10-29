0197d4567a851b91725312aad906e9e20a1083c8
pavan-bandi <109429441+pavan-bandi@users.noreply.github.com>
2022-10-30 02:02:02 +0530

    Update settings.py

--- cmp/settings.py
+++ cmp/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
 }
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
-DATABASE_URL=$(heroku config:get DATABASE_URL -a clmt) 
+DATABASE_URL=(heroku config:get DATABASE_URL -a clmt) 
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
